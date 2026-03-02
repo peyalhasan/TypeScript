be92707cfd5c0bdcdbdd9f702eaf7c2b617f2fbd
Peyal Hasan <peyalhasan143@gmail.com>
2026-03-02 13:23:17 +0600

    Inheritance

---
+++ dist/inheritance.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class User {
+    userName;
+    age;
+    constructor(userName, age) {
+        this.userName = userName;
+        this.age = age;
+    }
+    display() {
+        console.log(`username: ${this.userName}, age: ${this.age}`);
+    }
+}
+class Student extends User {
+    id;
+    department;
+    constructor(id, department, userName, age) {
+        super(userName, age);
+        this.id = id;
+        this.department = department;
+    }
+    display() {
+        console.log(`userId: ${this.id}, userName: ${this.userName}, age: ${this.age}, department: ${this.department}`);
+    }
+}
+let student1 = new Student(651256, 'CSE', "Peyal", 22);
+student1.display();
+//# sourceMappingURL=inheritance.js.map
